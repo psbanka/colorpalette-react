65b028d1ed58806b746f759d8a5db79aa0916669
Peter Banka <pbanka@fastly.com>
2020-10-22 14:57:53 -0700

    tweak with styling

--- src/Color.tsx
+++ src/Color.tsx
@@ -17,7 +17,9 @@ export default class Color extends Component<Props, {}> {
     return (
       <section className="color">
         <h1>{this.props.title}</h1>
-        <button onClick={this.props.onRemove}>X</button>
+        <button onClick={this.props.onRemove}>
+          <span role='img' aria-label='close'>✖️</span>
+        </button>
         <div className="color" style={{ backgroundColor: this.props.color }} />
         <div>
           <StarRating rating={this.props.rating} onRate={this.props.onRate} />
